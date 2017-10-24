d4b4b3fda8e4923a6d25df3a7c1e040695d8469b
EnriqueCarrizales <32001639+EnriqueCarrizales@users.noreply.github.com>
2017-10-24 12:31:33 -0400

    Mejora del menu principal, implementacion del boton de bluetooth.

--- app/src/main/java/corp/carrizales/hefesto_002/MainActivity.java
+++ app/src/main/java/corp/carrizales/hefesto_002/MainActivity.java
@@ -1,24 +1,11 @@
 package corp.carrizales.hefesto_002;
 
 import android.support.design.widget.TabLayout;
-import android.support.design.widget.FloatingActionButton;
-import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
-import android.support.v7.widget.Toolbar;
 
-import android.support.v4.app.Fragment;
-import android.support.v4.app.FragmentManager;
-import android.support.v4.app.FragmentPagerAdapter;
 import android.support.v4.view.ViewPager;
 import android.os.Bundle;
 import android.util.Log;
-import android.view.LayoutInflater;
-import android.view.Menu;
-import android.view.MenuItem;
-import android.view.View;
-import android.view.ViewGroup;
-
-import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -47,7 +34,7 @@ public class MainActivity extends AppCompatActivity {
 
     private void setupViewPager(ViewPager viewPager) {
         SectionsPageAdapter adapter = new SectionsPageAdapter(getSupportFragmentManager());
-        adapter.addFragment(new Mantenimiento_fragment(), "Mantenimiento");
+        adapter.addFragment(new Panel_fragment(), "Mantenimiento");
         adapter.addFragment(new Gasolina_fragment(), "Gasolina");
         adapter.addFragment(new Temperatura_fragment(), "Temperatura");
         adapter.addFragment(new Aceite_Fragment(), "Aceite");
--- app/src/main/java/corp/carrizales/hefesto_002/Panel_fragment.java
+++ app/src/main/java/corp/carrizales/hefesto_002/Panel_fragment.java
@@ -11,14 +11,14 @@ import android.view.ViewGroup;
  * Created by Luis on 23/10/2017.
  */
 
-public class Mantenimiento_fragment extends Fragment{
+public class Panel_fragment extends Fragment{
 
-    private static final String TAG = "Mantenimiento_fragment";
+    private static final String TAG = "Panel_fragment";
 
     @Nullable
     @Override
     public View onCreateView(LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
-        View view = inflater.inflate(R.layout.mantenimiento_fragment,container,false);
+        View view = inflater.inflate(R.layout.panel_fragment,container,false);
 
         return view;
     }
